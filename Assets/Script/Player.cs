49019e8281b26e201ac6f4726e63d3bab8ba7485
MossOcelot <108613206+MossOcelot@users.noreply.github.com>
2022-12-03 20:24:06 +0700

    aaaa

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -8,7 +8,8 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //agagagga
+        //agagagga
     }
 
     // Update is called once per frame
